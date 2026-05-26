f662631b44e1606c1d98e57d7cf12719bf82bfd1
jeevanavennam@outlook.com <jeevanavennam@outlook.com>
2026-05-26 17:26:10 +0530

    array

--- arrays.sh
+++ arrays.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-certificate=("AZ104" "AZ900" "AZ305")
-echo "first certificate $(certificate{0})"
-echo "Seconf certificate $(certificate{1})"
-echo "third certificate $(certificate{2})"
+certificate=("AZ104", "AZ900", "AZ305")
+echo "first certificate ${certificate[0]}"
+echo "second certificate ${certificate[1]}"
+echo "third certificate ${certificate[2]}"
 
-echo "print all the certificates: $(certificate{@})"
+echo "print all the certificates: ${certificate[@]}"
