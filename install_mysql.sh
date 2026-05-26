1466d044471d7bb67c4d2b1d275982acf8f0279a
jeevanavennam@outlook.com <jeevanavennam@outlook.com>
2026-05-26 18:02:58 +0530

    array

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 user_id=$(id -u)
-if [$user_id -ne 0]
+if [ $user_id -ne 0 ]
 then
     echo "Please run script as root"
+    exit 1
 else
     echo "you are in root access"
 fi
 validate() {
-    if [$1 -eq 0]
+    if [ $1 -eq 0 ]
     then
     echo "$2 is installed"
     else
     echo "$2 is not installed"
-    exit1
+    exit 1
     fi
 }
 dnf list installed mysql
-if [$? =ne 0]
+if [ $? -ne 0 ]
 then
     echo "installing mysql"
     dnf install mysql-server -y
@@ -25,7 +26,7 @@ else
 echo "mysql is already installed"
 fi
 dnf list installed nginx
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then
     echo "nginx not installed"
     dnf install nginx -y
