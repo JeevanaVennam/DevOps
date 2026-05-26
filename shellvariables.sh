081f1a9b394fc2fbcc6fb7d3fa6431a3c3885b05
jeevanavennam@outlook.com <jeevanavennam@outlook.com>
2026-05-26 17:42:48 +0530

    array

--- shellvariables.sh
+++ shellvariables.sh
@@ -4,6 +4,6 @@ echo "Number of variables: $#"
 echo "Script name: $0"
 echo "PID at the background: $!"
 echo "PID $$"
-echo "current directory:" $pwd
+echo "current directory: $(pwd)"
 sleep 10 &
-echo "user running the script:" $user
+echo "user running the script:$(user)"
