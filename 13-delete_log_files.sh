b5094cffe2ac6588dd696547f331cbd21221f087
jeevanavennam@outlook.com <jeevanavennam@outlook.com>
2026-05-27 17:02:37 +0530

    devops

--- 13-delete_log_files.sh
+++ 13-delete_log_files.sh
@@ -3,5 +3,5 @@ source_dir=("/home/ec2-user/app-logs")
 files_to_delete=$(find $source_dir . -name "*.log" -mtime +14)
 while IFS= read -r filepath
 do
-    rm-rf $filepath
+    rm -rf $filepath
 done<<<"files_to_delete"
